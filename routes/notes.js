9bab7b8a891d70c8d50e9fae942e70a7d92d8851
LeonathaTurtle <mancillh@gmail.com>
2024-08-04 18:41:36 -0500

    edited notes.js to fix broken path

--- routes/notes.js
+++ routes/notes.js
@@ -1,34 +1,34 @@
-const router = require('express').Router()
-const db = require('../db/db.json')
-const fs = require('fs')
+const notes = require('express').Router()
 const uuid = require('../helpers/uuid');
 
+// Helper functions for reading and writing to the JSON file
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
 //GET route to read the db.JSON file and return all saved notes as JSON
-router.get('/notes', (req, res) => {
+notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile(db).then((data) => res.json(JSON.parse(data)));
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 //POST Route for adding notes to notes page --> should receive a new note to save on the request body, add it to 
 //the db.json file, and then return the new note to the client
-router.post('/notes', (req, res) => {
+notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
-    console.log(req.body);
       
     const { title, text } = req.body;
       
-    if (title && text) {
+    if (req.body) {
       const newNote = {
         title,
         text,
-        tip_id: uuid(),
+        note_id: uuid(),
       };
       
-      readAndAppend(newNote, './db/tips.json');
+      readAndAppend(newNote, '../db/db.json');
       res.json(`Note added successfully`);
       } else {
         res.error('Error in adding note');
       }
     });
 
-module.exports = router
+module.exports = notes;
