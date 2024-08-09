14ddca91ada694819a23fba2399b8a6f936d6c43
LeonathaTurtle <mancillh@gmail.com>
2024-08-09 15:31:00 -0500

    delete route working, READ ME added

---
+++ README.md
@@ -0,0 +1,45 @@
+# Note Taker
+  [![MIT License](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
+  ## Description
+  This app can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+  ## Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Features](#features)
+  * [How to Contribute](#how-to-contribute)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  N/A
+
+  ## Usage
+  Write notes to organize thoughts and keep track of tasks you need to complete. Managers, team leads, and students may also use this app to keep everyone on track.
+
+  ## Credits
+  N/A
+
+  ## License
+  This application is covered under MIT License
+  [MIT License Link](https://opensource.org/license/MIT)
+
+  ## Features
+  N/A
+
+  ## How to Contribute
+  Fork the repo. Make changes. Push changes.
+
+  ## Tests
+  N/A
+
+  ## Questions
+  My GitHub username: mancillh 
+
+  My GitHub profile: https://github.com/mancillh 
+
+  For additional questions, contact me at: mancillh@gmail.com
--- routes/notes.js
+++ routes/notes.js
@@ -8,75 +8,73 @@ const { readFromFile, writeToFile } = require('../helpers/fsUtils');
 let data = db;
 
 //GET Route to read the db.JSON file and return all saved notes as JSON
-  notes.get ('/', (req, res) => {
-    console.info(`${req.method} request received to view notes`);
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received to view notes`);
+  readFromFile('../Note Taker/db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      res.json(JSON.parse(data));
+    }
+  })
+});
+
+//POST Route for adding notes to notes page --> receives a new note to save on the request body, adds it to 
+//the db.json file, and then return the new note to the client
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the note object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    // Obtain existing notes
     readFromFile('../Note Taker/db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
         // Convert string into JSON object
-        res.json(JSON.parse(data));
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        writeToFile(
+          '../Note Taker/db/db.json',
+          parsedNotes, null, 4,
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
       }
-    })
-  });
+    });
 
-//POST Route for adding notes to notes page --> receives a new note to save on the request body, adds it to 
-//the db.json file, and then return the new note to the client
-  notes.post ('/', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
 
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the note object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
-  
-      // Obtain existing notes
-      readFromFile('../Note Taker/db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new note
-          parsedNotes.push(newNote);
-  
-          // Write updated notes back to the file
-          writeToFile(
-            '../Note Taker/db/db.json',
-            parsedNotes, null, 4,
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(data);
-    } else {
-      res.status(500).json('Error in posting note');
-    }
-  });
+    console.log(response);
+    res.status(201).json(data);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
 
 // Delete Route
 notes.delete('/:id', (req, res) => {
   console.info(`${req.method} request received to delete note`);
-
-  const id = req.params.id;
 
   readFromFile('../Note Taker/db/db.json', 'utf8', (err, data) => {
     if (err) {
@@ -85,19 +83,25 @@ notes.delete('/:id', (req, res) => {
       // Convert string into JSON object
       let parsedNotes = JSON.parse(data);
 
-      { parsedNotes[i] } = parsedNotes.filter(/:id === note_id);
-      
-      // Write updated notes back to the file
-      writeToFile(
-        '../Note Taker/db/db.json',
-        parsedNotes, null, 4,
-        (writeErr) =>
-          writeErr
-            ? console.error(writeErr)
-            : res.json({ message:'Successfully deleted note' })
-      )
-      }
+      const { id } = req.params;
+      const notesLessOne = parsedNotes.filter(newNote => newNote.id != id);
+
+    writeToFile('../Note Taker/db/db.json', notesLessOne, null, 4,
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : res.json(notesLessOne)
+          )
+        }
     })
-});
+    readFromFile('../Note Taker/db/db.json', 'utf8', async (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        await res.json(JSON.parse(data));
+      }
+    });
+  });
 
 module.exports = notes; 
